5b95b52b126d95a95649b6e545f11c4f98452913
Takeya Shimizu <takeya.shimizu@sportsbull.jp>
2022-01-29 16:46:13 +0900

    Update: Launch camera and Image Picker

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +16,27 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var photoImage: UIImageView!
+
     @IBAction func cameraLaunchAction(_ sender: Any) {
+        // カメラにアクセスできるか
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated: true, completion: nil)
+        } else {
+            print("Camera is not available.")
+        }
     }
+
     @IBAction func shareAction(_ sender: Any) {
+    }
+
+    // 写真を撮った後に呼ばれる delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImage.image = info[.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
     }
 }
 
